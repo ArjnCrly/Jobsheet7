b20945d72c1f693eb335cf45b1146d9945cf982a
ArjnCrly <arjuna.carly13042gmail.com>
2023-10-20 08:20:54 +0700

    DoWhileCuti

---
+++ DoWhileCuti04.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class DoWhileCuti04 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int jatahCuti, jumlahHari;
+        String konfirmasi;
+
+        System.out.print("Jatah Cuti: ");
+        jatahCuti = scan.nextInt();
+
+        do {
+        System.out.print("Apakah anda ingin mengambil cuti (y/t) : ");
+        konfirmasi = scan.next();
+
+        if (konfirmasi.equalsIgnoreCase("y")) {
+        System.out.print("Jumlah Hari: ");
+        jumlahHari = scan.nextInt();
+
+            if (jumlahHari <= jatahCuti) {
+            jatahCuti -= jumlahHari;
+            System.out.println("Sisa jatah cuti: " + jatahCuti);
+            } else {
+            System.out.println("Sisa jatah cuti anda tidak mencukupi");
+            break;
+            }
+        }
+    }
+        while (jatahCuti > 0);
+        scan.close();
+    }
+}
